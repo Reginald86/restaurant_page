dfea3725cd55e243ed47cf0913b8514f13f26f16
Reginald86 <49849893+Reginald86@users.noreply.github.com>
2024-10-21 15:38:02 -0500

    menu page items added

--- src/menu.js
+++ src/menu.js
@@ -26,9 +26,37 @@ export function menu() {
     menu2.className = "menuSection";
     menu2.id = "menu2";
 
+    const entree = document.createElement('h2');
+    entree.textContent = "Entrées:"
+    entree.className = "menuTitle";
+    const entreeItems = document.createElement("ul");
+    entreeItems.className = "menuItems"
+    entreeItems.innerHTML = `
+          <li>Grilled Salmon and Bone Broth Rice - $27</li>
+          <li>Filet Woofnon and Sweet Potatoes - $25</li>
+          <li>Turkey and Biscuits with Gravy - $21</li>
+          <li>Beefy Bone Stew - $18</li>
+          <li>Lamb Lickin' Meatballs - $23</li>
+          <li>Pup-eroni Pizza - $18</li>
+      `;
+
+      const appetizer = document.createElement('h2');
+      appetizer.textContent = "Appetizers:"
+      appetizer.className = "menuTitle";
+      appetizer.id = "appetizer"
+      const appetizerItems = document.createElement("ul");
+      appetizerItems.className = "menuItems"
+      appetizerItems.innerHTML = `
+            <li>Gourment Milkbones - $11</li>
+            <li>Cheesy Paws Bites - $14</li>
+            <li>Mini Meat Muffins - $16</li>
+        `;
+
+
 
 
    subContainer.append(menu1, menu2);
+   menu1.append(entree, entreeItems, appetizer, appetizerItems)
    container.append(title, subTitle, subContainer);
    content.appendChild(container);
 
